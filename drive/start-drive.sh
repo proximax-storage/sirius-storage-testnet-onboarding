2fa9ee2dca2360cd4f6572a18f504c1c4f90f657
Shin Neng <48303181+shinneng@users.noreply.github.com>
2020-03-03 22:29:03 +0800

    update image and config to v0.6.4.1 (#5)
    * update image and config to v0.6.4.1
    
    * fix var in bash script

--- drive/start-drive.sh
+++ drive/start-drive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$drive_key = "<PRIVATE_KEY>"
+drive_key='<PRIVATE_KEY>'
 
 docker stop drive
 docker rm drive
-docker run -d --name drive -p 6366:6466 -p 63666:64666 -v $PWD:/root/.dfms:rw proximax/sirius-storage-drive:v0.6.3 dfms -k $drive_key
+docker run -d --name drive -p 6366:6466 -p 63666:64666 -v $PWD:/root/.dfms:rw proximax/sirius-storage-dfms:v0.6.4.1 dfms -k $drive_key
