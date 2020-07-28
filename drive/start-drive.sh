a0df600258d86ac8cd63bee7216e7f15b4ee32a0
shinneng <shinneng@proximax.io>
2020-07-28 10:38:15 +0800

    update img to v0.7.02

--- drive/start-drive.sh
+++ drive/start-drive.sh
@@ -4,4 +4,4 @@ drive_key='<PRIVATE_KEY>'
 
 docker stop drive
 docker rm drive
-docker run -d --name drive -p 6366:6466 -p 63666:64666 -v $PWD:/root/.dfms:rw proximax/sirius-storage-dfms:v0.6.4.1 dfms -k $drive_key
+docker run -d --name drive -p 6366:6466 -p 63666:64666 -v $PWD:/root/.dfms:rw proximax/sirius-storage-dfms:v0.7.0.2 dfms -k $drive_key
--- replicator/start-replicator.sh
+++ replicator/start-replicator.sh
@@ -4,5 +4,5 @@ replicator_key='<PRIVATE_KEY>'
 
 docker stop replicator
 docker rm replicator
-docker run -d --name replicator --network host -v $PWD:/root/.dfmsr:rw proximax/sirius-storage-dfmsr:v0.6.4.1 dfmsr -k $replicator_key
+docker run -d --name replicator --network host -v $PWD:/root/.dfmsr:rw proximax/sirius-storage-dfmsr:v0.7.0.2 dfmsr -k $replicator_key
 docker exec replicator dfms contract accepting
